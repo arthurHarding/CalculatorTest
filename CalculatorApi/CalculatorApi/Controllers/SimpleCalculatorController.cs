7f1354607f603bd3d286e74cb0d60754d9d7d7cf
Arthur Harding <arthurh1@hotmail.co.uk>
2023-07-10 13:37:02 +0100

    Add calculator web API

---
+++ CalculatorApi/CalculatorApi/Controllers/SimpleCalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CalculatorTest;
+
+namespace CalculatorWebService.Controllers
+{
+    [Route("api/simplecalculator")]
+    [ApiController]
+    public class SimpleCalculatorController
+    {
+        [HttpGet("add/{start}/{amount}")]
+        public int Add(int start, int amount)
+        {
+            var calculator = new SimpleCalculator();
+            return calculator.Add(start, amount);
+        }
+
+        [HttpGet("subtract/{start}/{amount}")]
+        public int Subtract(int start, int amount)
+        {
+            var calculator = new SimpleCalculator();
+            return calculator.Subtract(start, amount);
+        }
+    }
+}
